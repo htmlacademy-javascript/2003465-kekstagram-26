5f801b9a848d8066ee1fe2adeac3d30224cea579
Artem Simanov <metall704@yandex.ru>
2022-06-25 16:13:46 +0300

    добавил функции по обработке большого фото

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 import './insert.js';
+import {closeBigPicture} from './big-picture.js';
+
+const BIG_PICTURE_CANCEL = document.querySelector('.big-picture__cancel');
+
+BIG_PICTURE_CANCEL.addEventListener('click', closeBigPicture);
+
+
