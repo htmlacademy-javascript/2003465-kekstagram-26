6051861f6c91c63c2c2147db706261ed75868d79
Artem Simanov <metall704@yandex.ru>
2022-07-06 21:22:39 +0300

    добавлены функции по показу всех комментариев

--- js/big-picture.js
+++ js/big-picture.js
@@ -2,18 +2,21 @@ import {PHOTOS} from './insert.js';
 
 const BIG_PICTURE = document.querySelector('.big-picture');
 const BIG_PICTURE_IMG =  BIG_PICTURE.querySelector('.big-picture__img > img');
-
+const COMMENTS_NUMBER = document.querySelector('.comments-number');
 const LIKES_COUNT = BIG_PICTURE.querySelector('.likes-count');
 const COMMENTS_COUNT = BIG_PICTURE.querySelector('.comments-count');
-const SOCIAL_COMMENT_COUNT = BIG_PICTURE.querySelector('.social__comment-count');
-const COMMENTS_LOADER = BIG_PICTURE.querySelector('.comments-loader');
+const SOCIAL_COMMENTS = document.querySelector('.social__comments');
+const COMMENTS_LOADER = document.querySelector('.comments-loader');
 const PHOTO_DESCRIPTION = BIG_PICTURE.querySelector('.social__caption');
 const BIG_PICTURE_CANCEL = document.querySelector('.big-picture__cancel');
+let lastComment = 5;
 
 function closeBigPicture() {
   BIG_PICTURE.classList.add('hidden');
   document.body.classList.remove('modal-open');
   BIG_PICTURE_CANCEL.removeEventListener('click', closeBigPicture);
+  COMMENTS_LOADER.removeEventListener('click', addYetFiveComments);
+  COMMENTS_LOADER.classList.remove('hidden');
 }
 
 function closeBigPictureByButton () {
@@ -26,11 +29,6 @@ function closeBigPictureByButton () {
   });
 }
 
-function hideComments () {
-  SOCIAL_COMMENT_COUNT.classList.add('hidden');
-  COMMENTS_LOADER.classList.add('hidden');
-}
-
 function showBigPicture (picture) {
   const PICTURE_LIKES = picture.querySelector('.picture__likes');
   const PICTURE_COMMENTS = picture.querySelector('.picture__comments');
@@ -38,6 +36,7 @@ function showBigPicture (picture) {
   BIG_PICTURE_IMG.src = picture.querySelector('.picture__img').src;
   LIKES_COUNT.textContent = PICTURE_LIKES.textContent;
   COMMENTS_COUNT.textContent = PICTURE_COMMENTS.textContent;
+  COMMENTS_LOADER.addEventListener('click', addYetFiveComments);
 }
 
 function randerCommentsList (index) {
@@ -63,4 +62,33 @@ function stopBodyScroll() {
   document.body.classList.add('modal-open');
 }
 
-export {closeBigPictureByButton, hideComments, showBigPicture, randerCommentsList, setPhotoDescription, stopBodyScroll};
+function limitCommentsNumber () {
+  if (SOCIAL_COMMENTS.children.length > 5) {
+    for (let i = 5; i < SOCIAL_COMMENTS.children.length; i++) {
+      SOCIAL_COMMENTS.children[i].classList.add('hidden');
+    }
+  }
+}
+
+function addYetFiveComments () {
+  if ((SOCIAL_COMMENTS.children.length - lastComment) < 5) {
+    for (let i = lastComment; i < SOCIAL_COMMENTS.children.length; i++) {
+      SOCIAL_COMMENTS.children[i].classList.remove('hidden');
+    }
+    COMMENTS_LOADER.classList.add('hidden');
+    COMMENTS_NUMBER.textContent = SOCIAL_COMMENTS.children.length;
+    lastComment = 5;
+    return;
+  }
+  for (let i = lastComment; i < lastComment + 5; i++) {
+    SOCIAL_COMMENTS.children[i].classList.remove('hidden');
+  }
+  lastComment += 5;
+  COMMENTS_NUMBER.textContent = lastComment;
+}
+
+function showCommentsNumber () {
+  COMMENTS_NUMBER.textContent = 5;
+}
+
+export {closeBigPictureByButton, showBigPicture, randerCommentsList, setPhotoDescription, stopBodyScroll, showCommentsNumber, limitCommentsNumber};
--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 import './insert.js';
 import './form-img.js';
-import {closeBigPictureByButton, hideComments, showBigPicture, randerCommentsList, setPhotoDescription, stopBodyScroll} from './big-picture.js';
+import {closeBigPictureByButton, showBigPicture, randerCommentsList, setPhotoDescription, stopBodyScroll, showCommentsNumber, limitCommentsNumber} from './big-picture.js';
 
 const PICTURES = document.querySelectorAll('.picture');
 
 PICTURES.forEach((PICTURE, i) => {
   PICTURE.addEventListener('click', showBigPicture.bind(null, PICTURE));
-  PICTURE.addEventListener('click', hideComments);
   PICTURE.addEventListener('click', randerCommentsList.bind(null, i));
   PICTURE.addEventListener('click', setPhotoDescription.bind(null, i));
+  PICTURE.addEventListener('click', showCommentsNumber);
+  PICTURE.addEventListener('click', limitCommentsNumber);
   PICTURE.addEventListener('click', stopBodyScroll);
   PICTURE.addEventListener('click', closeBigPictureByButton);
 });
