65d1350629c12cfba0ca7b70377cf7d9481fb14f
Keks <keks@htmlacademy.ru>
2022-07-12 20:28:28 +0300

    :heavy_check_mark: Сборка #11

--- 11/js/main.js
+++ 11/js/main.js
@@ -1,13 +1,13 @@
 import './insert.js';
 import './form-img.js';
 import './formatting-photo.js';
-import {closeBigPictureByButton, showBigPicture, renderCommentsList, setPhotoDescription, stopBodyScroll, showCommentsNumber, limitCommentsNumber} from './big-picture.js';
+import {closeBigPictureByButton, showBigPicture, randerCommentsList, setPhotoDescription, stopBodyScroll, showCommentsNumber, limitCommentsNumber} from './big-picture.js';
 
 const PICTURES = document.querySelectorAll('.picture');
 
 PICTURES.forEach((PICTURE, i) => {
   PICTURE.addEventListener('click', showBigPicture.bind(null, PICTURE));
-  PICTURE.addEventListener('click', renderCommentsList.bind(null, i));
+  PICTURE.addEventListener('click', randerCommentsList.bind(null, i));
   PICTURE.addEventListener('click', setPhotoDescription.bind(null, i));
   PICTURE.addEventListener('click', showCommentsNumber);
   PICTURE.addEventListener('click', limitCommentsNumber);
