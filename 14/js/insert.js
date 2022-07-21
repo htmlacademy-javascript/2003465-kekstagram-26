afd58d4f1b87efa85edfc2fa6646cb7c552f9e28
Keks <keks@htmlacademy.ru>
2022-07-21 20:35:20 +0300

    :heavy_check_mark: Сборка #14

---
+++ 14/js/insert.js
@@ -0,0 +1,18 @@
+const PICTURE_LIST = document.querySelector('.pictures');
+const PICTURE_TEMPLATE = document.querySelector('#picture').content;
+const PICTURE = PICTURE_TEMPLATE.querySelector('a');
+const photoFragment = document.createDocumentFragment();
+
+function insertPhoto (photos) {
+  photos.forEach(({url, comments, likes}) => {
+    const PICTURE_ITEM = PICTURE.cloneNode(true);
+    PICTURE_ITEM.querySelector('.picture__img').src = url;
+    PICTURE_ITEM.querySelector('.picture__comments').textContent = comments.length;
+    PICTURE_ITEM.querySelector('.picture__likes').textContent = likes;
+    photoFragment.append(PICTURE_ITEM);
+  });
+  PICTURE_LIST.append(photoFragment);
+  return photos;
+}
+
+export {insertPhoto};
